116dd5b08d8ad5978f66490f4c640062ed333d8c
Brendan Musick <bmuze1@gmail.com>
2024-04-23 08:22:45 -0600

    last try today

--- src/pages/BlogPage.tsx
+++ src/pages/BlogPage.tsx
@@ -1,12 +1,12 @@
 import BlogCard from '../components/BlogCard.tsx';
 import { Link } from 'react-router-dom';
-import { filenames } from '/src/data.json'
+import { filenames } from '/data.json'
 
 import useFetchFileData from '../hooks/useFetchFileData.tsx';
 
 function BlogPage(){
     const blogs = filenames.map((filename, index)=>{
-        const fileSrc = `/src/blogs/${filename}`;
+        const fileSrc = `/blogs/${filename}`;
         const [post, metadata] = useFetchFileData(fileSrc);
         
         return (
