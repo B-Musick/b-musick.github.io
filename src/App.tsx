b5cf18bfc1da6b772d5db1cd71dbecdb4417985e
Brendan Musick <bmuze1@gmail.com>
2024-04-24 10:03:01 -0600

    Switched to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
