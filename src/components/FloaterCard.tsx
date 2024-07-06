da57c70ef9bde98ca8233e3b0af9a560f0f53d52
B-Musick <bmuze1@gmail.com>
2024-07-06 12:45:22 -0500

    Fixed floater card color visibility

--- src/components/FloaterCard.tsx
+++ src/components/FloaterCard.tsx
@@ -3,7 +3,7 @@ import { FloaterCardProps } from "../lib/types";
 
 const FloaterCard: React.FC<FloaterCardProps> = ({skill}) =>{
     return (
-        <div className="flex text-xs items-center px-2 my-1 rounded-full bg-white/5 w-fit invert backdrop-opacity-90 mr-1">
+        <div className="flex text-xs items-center px-2 my-1 rounded-full bg-white text-black w-fit mr-1">
             {skill}
         </div>
     )
