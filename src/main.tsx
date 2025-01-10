5425713274c4ae8f3816d29bb3d4b4bb25cdd3ac
B-Musick <bmuze1@gmail.com>
2025-01-09 21:07:44 -0600

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.js'
 import './index.css'
-import { BrowserRouterRouter as Router, Route, Routes, useLocation, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation, BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
