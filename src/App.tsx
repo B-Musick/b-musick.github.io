3a0fb9c93a8d894998cffb214c591830c570860a
B-Musick <bmuze1@gmail.com>
2025-01-20 20:35:49 -0600

    Switched back to hash router till finish new portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.js'
 import './index.css'
-import { BrowserRouter as Router, Route, Routes, useLocation, BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, useLocation, BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
 )
