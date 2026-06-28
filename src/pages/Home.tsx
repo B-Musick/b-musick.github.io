bdbf994dbdd07ff2801fd4c60ce7f168c3834e9d
B-Musick <bmuze1@gmail.com>
2026-06-28 18:10:02 -0500

    Switched to hash router

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,8 +11,8 @@ export default function Home() {
           Full Stack Developer
         </h2>
         <div className="flex justify-evenly w-full">
-          <Button url={"/about"} text={"About Me"} />
-          <Button url={"/projects"} text={"View My Projects"} />
+          <Button url={"/#/about"} text={"About Me"} />
+          <Button url={"/#/projects"} text={"View My Projects"} />
         </div>
       </div>
       <div className="fixed z-[0] h-full w-1/2 right-0 overflo">
@@ -21,14 +21,14 @@ export default function Home() {
       <div className="fixed bottom-0 w-full sm:w-2/3 flex justify-evenly items-end">
         <BottomCard
           classes="flex-1"
-          url="/projects"
+          url="/#/projects"
           imageUrl="/imgs/icons/ucn.png"
           title="UCN Website Developer"
           description="I am the website administrator and developer for University College of the North. Learn more here."
         />
         <BottomCard
           classes="flex-1"
-          url="/computer-science"
+          url="/#/computer-science"
           imageUrl="/imgs/icons/umanitoba.svg"
           title="Computer Science Major"
           description="I graduated with a Computer Science Degree in 2022. Learn more about my time at University."
